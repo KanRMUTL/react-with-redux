5fc905b0f1179be017859ec4e695d8239b7fb3db
Kiadtisak Lowongsa <kanlowongsa20@gmail.com>
2022-02-12 15:01:23 +0700

    created base variables

---
+++ src/constant/menuList.ts
@@ -0,0 +1,22 @@
+import { Menu } from 'types/Menu';
+
+const menuList: Menu[] = [
+  {
+    name: 'Popular',
+    active: true,
+  },
+  {
+    name: 'Trending',
+    active: false,
+  },
+  {
+    name: 'TV',
+    active: false,
+  },
+  {
+    name: 'Playing Now',
+    active: false,
+  },
+];
+
+export default menuList;
